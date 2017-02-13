c7cd8be6263b333c3755c23cf81f0f945bf5441c
Mattia Procopio <promat85@gmail.com>
2017-02-13 14:57:35 +0100

    Wire the pump code

--- main.c
+++ main.c
@@ -4,76 +4,99 @@
 
 #include <wiringPi.h>
 
-#define GPIO23     4
-#define GPIO24     5
+#define GPIO23      4;
+#define GPIO24      5;
+#define GPIO20      28;
+static int TRIGGER_PIN    = GPIO23;
+static int ECHO_PIN       = GPIO24;
+static int PUMP_PIN       = GPIO20;
+static double SOUND_SPEED = 340.29;
+static int MAX_READS      = 30;
+struct timespec start_time;
+struct timespec end_time;
 
-static int trigger = GPIO23;
-static int echo = GPIO24;
-struct timespec startTimeUsec;
-struct timespec endTimeUsec;
 
-
-void recordPulseLength (void) {
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &startTimeUsec);
-    while ( digitalRead(echo) == HIGH );
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &endTimeUsec);;
+void record_pulse_length (void) {
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_time);
+  while ( digitalRead(ECHO_PIN) == HIGH );
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_time);;
 }
 
-float get_distance() {
-  double sound_speed = 340.29;
+double get_distance() {
+  double travel_time = 0.0;
+  double distance    = 0.0;
 
-  pinMode(trigger, OUTPUT);
-  pinMode(echo, INPUT);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 
-  // Initialize the sensor's trigger pin to low. If we don't pause
-  // after setting it to low, sometimes the sensor doesn't work right.
-  digitalWrite(trigger, LOW);
+  /* Initialize the sensor's trigger pin to low. If we don't pause
+     after setting it to low, sometimes the sensor doesn't work right. */
+  digitalWrite(TRIGGER_PIN, LOW);
   delay(500); // .5 seconds
 
-  // Triggering the sensor for 10 microseconds will cause it to send out
-  // 8 ultrasonic (40Khz) bursts and listen for the echos.
-  digitalWrite(trigger, HIGH);
+  /* Triggering the sensor for 10 microseconds will cause it to send out
+     8 ultrasonic (40Khz) bursts and listen for the echos. */
+  digitalWrite(TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigger, LOW);
+  digitalWrite(TRIGGER_PIN, LOW);
 
-  // The sensor will raise the echo pin high for the length of time that it took
-  // the ultrasonic bursts to travel round trip.
-  while ( digitalRead(echo) == LOW);
-  recordPulseLength();
+  /* The sensor will raise the echo pin high for the length of time that it took
+     the ultrasonic bursts to travel round trip.*/
+  while ( digitalRead(ECHO_PIN) == LOW);
+  record_pulse_length();
 
-  long travelTimeUsec = endTimeUsec.tv_nsec - startTimeUsec.tv_nsec;
-  double distanceMeters = ((travelTimeUsec/1000000000.0)*sound_speed)/2;
+  travel_time = end_time.tv_nsec - start_time.tv_nsec;
+  distance = ((travel_time/1000000000.0) * SOUND_SPEED)/2;
 
-  return distanceMeters * 100;
+  return distance * 100;
+}
+
+void refill_water()
+{
+  /* Turn on the pump and refill for 3 sec */
+  digitalWrite(PUMP_PIN, LOW);
+  sleep(3);
+  digitalWrite(PUMP_PIN, HIGH);
+}
+
+int setup_pump()
+{
+  pinMode(PUMP_PIN, OUTPUT);
+  digitalWrite(PUMP_PIN, HIGH);
+
+return 0;
 }
 
 
 int main()
 {
   int x;
-  float measure, sum, read_distance_array [10];
+  double average_distance, measure, sum, read_distance_array [30];
 
-
-  // Initialise the GPIO
   wiringPiSetup();
+  setup_pump();
   sleep(2);
 
   while(1) {
     // Wait 30 sec between one measurement and the other
     sleep(30);
-    // Take 10 measures and find the average
-    for (x = 0; x < 10; x++) {
+    // Take 30 measures and find the average
+    for (x = 0; x < MAX_READS; x++) {
       sum = 0;
-      sleep(0.5);
+      usleep(100000);
       measure = get_distance();
       // Push the read to the array
       read_distance_array[x] = measure;
     }
-    for (x = 0; x < 10; x++) {
+    for (x = 0; x < MAX_READS; x++) {
       sum = sum + read_distance_array[x];
     }
-
-    printf("Average distance: %.2f\n", sum / 10);
+    average_distance = sum / 30;
+    printf("Average distance: %.2f\n", average_distance);
+    if (average_distance > 4.00) {
+      printf("Refilling the tank\n");
+      refill_water();
+    }
   }
 
   return 0;
